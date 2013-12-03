5859f4195b2fca78a59008fe2bf0136c341bc1f1
Rob Halff <rob.halff@gmail.com>
2013-12-03 21:01:28 +0100

    inital version

---
+++ nodes/connect/node.js
@@ -0,0 +1,24 @@
+var consumer = new datashift(input.username, input.api_key);
+consumer.connect();
+
+//Emitted when stream is connected
+if(input.connect) consumer.on('connect', input.connect);
+
+//Emitted when there is an error
+if(input.error) consumer.on('error', input.error);
+
+//Emitted when there is a warning
+if(input.warning) consumer.on('warning', input.warning);
+
+//Emitted when disconnected
+if(iput.disconnect) consumer.on('disconnect', input.disconnect);
+
+//Emitted when an interaction is received
+if(input.interaction) consumer.on('interaction', input.interaction);
+
+//Emitted when a delete message is received
+if(input.delete) consumer.on('delete', input.delete);
+
+output = {
+  consumer: consumer
+};
