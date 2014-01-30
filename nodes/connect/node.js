5335b3562a7bcbf028d5df764dce83cfa9c8ff8f
Rob Halff <rob.halff@gmail.com>
2014-01-30 23:01:54 +0100

    beautify

--- nodes/connect/node.js
+++ nodes/connect/node.js
@@ -2,22 +2,22 @@ var consumer = new datasift(input.username, input.api_key);
 consumer.connect();
 
 //Emitted when stream is connected
-if(input.connect) consumer.on('connect', input.connect);
+if (input.connect) consumer.on('connect', input.connect);
 
 //Emitted when there is an error
-if(input.error) consumer.on('error', input.error);
+if (input.error) consumer.on('error', input.error);
 
 //Emitted when there is a warning
-if(input.warning) consumer.on('warning', input.warning);
+if (input.warning) consumer.on('warning', input.warning);
 
 //Emitted when disconnected
-if(iput.disconnect) consumer.on('disconnect', input.disconnect);
+if (iput.disconnect) consumer.on('disconnect', input.disconnect);
 
 //Emitted when an interaction is received
-if(input.interaction) consumer.on('interaction', input.interaction);
+if (input.interaction) consumer.on('interaction', input.interaction);
 
 //Emitted when a delete message is received
-if(input.delete) consumer.on('delete', input.delete);
+if (input.delete) consumer.on('delete', input.delete);
 
 output = {
   consumer: consumer
